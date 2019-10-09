e4781e21f8d43f31e8be13618035bceef5a1700a
haykalrayyan99 <haykalrayyan43@gmail.com>
2019-10-10 00:27:32 +0700

    Menambahkan Splash Screen

--- app/src/main/java/com/rayyan/kedaioutdoorshelter/MainActivity.java
+++ app/src/main/java/com/rayyan/kedaioutdoorshelter/MainActivity.java
@@ -2,7 +2,9 @@ package com.rayyan.kedaioutdoorshelter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void healderSewaTenda(View view) {
+        Intent intent = new Intent(this, Tenda.class);
+        startActivity(intent);
+    }
+
+    public void healderSewaCarrier(View view) {
+        Intent intent = new Intent(this, Carrier.class);
+        startActivity(intent);
+    }
+
+    public void healderMatras(View view) {
+        Intent intent = new Intent(this, Matras.class);
+        startActivity(intent);
+    }
+
+    public void healderSleepingBag(View view) {
+        Intent intent = new Intent(this, SleepingBag.class);
+        startActivity(intent);
+    }
+
+    public void healderExit(View view) {
+        finish();
     }
 }
---
+++ app/src/main/java/com/rayyan/kedaioutdoorshelter/Tenda.java
@@ -0,0 +1,16 @@
+package com.rayyan.kedaioutdoorshelter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Tenda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenda);
+    }
+}
